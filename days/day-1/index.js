7a090680971dcba396a75927494dc0778c0248ad
Ben Merken <ben.merken@gmail.com>
2025-12-26 01:13:14 +0100

    WIP - day 1, part 2 -- looking for the needle in the haystack...

--- days/day-1/index.js
+++ days/day-1/index.js
@@ -10,18 +10,28 @@ const input = parseInput(inputPath);
 
 let reading = 50;
 let timesZeroHit = 0;
+let timesZeroPassed = 0;
 
 input.forEach((line) => {
-  const [direction, value] = [line[0], parseInt(line.slice(1))];
+  const [direction, value] = [line[0], parseInt(line.slice(1), 10)];
+  const previousReading = reading;
 
   if (direction === "L") {
     reading -= value;
   } else {
     reading += value;
   }
+
+  timesZeroPassed += Math.abs(Math.floor(reading / 100));
+  if (previousReading === 0 && reading < 0) timesZeroPassed--;
+
+  reading = ((reading % 100) + 100) % 100;
+
   if (Math.abs(reading % 100) === 0) timesZeroHit++;
 
   console.log(`Current reading: ${reading}`);
+  console.log(`Times passed zero: ${timesZeroPassed}`);
 });
 
-console.log("Day 1 solution:", timesZeroHit);
+console.log("Day 1 part 2 solution:", timesZeroHit);
+console.log("Day 1 part 2 solution:", timesZeroHit + timesZeroPassed);
