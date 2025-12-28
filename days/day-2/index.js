c897dcbfc8cba4ca1e82318f0e1a920c29244dda
Ben Merken <ben.merken@gmail.com>
2025-12-28 19:13:56 +0100

    Finish day 2, part 2

--- days/day-2/index.js
+++ days/day-2/index.js
@@ -8,15 +8,19 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const inputPath = path.join(__dirname, "input.txt");
 const ranges = parseInput(inputPath)[0].split(",");
 
-const invalidRegEx = new RegExp(/^(\d*)\1$/);
-let invalidIdsSum = 0;
+const part1InvalidRegEx = new RegExp(/^(\d+)\1$/);
+const part2InvalidRegEx = new RegExp(/^(\d+)\1\1*$/);
+let part1InvalidIdsSum = 0;
+let part2InvalidIdsSum = 0;
 
 ranges.forEach((range) => {
   const [start, end] = range.split("-").map((num) => parseInt(num, 10));
 
   for (let i = start; i <= end; i++) {
-    if (invalidRegEx.test(i.toString())) invalidIdsSum += i;
+    if (part1InvalidRegEx.test(i.toString())) part1InvalidIdsSum += i;
+    if (part2InvalidRegEx.test(i.toString())) part2InvalidIdsSum += i;
   }
 });
 
-console.log("Day 2 part 1 solution:", invalidIdsSum);
+console.log("Day 2 part 1 solution:", part1InvalidIdsSum);
+console.log("Day 2 part 2 solution:", part2InvalidIdsSum);
